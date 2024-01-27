39d3a0c16311ef89e94d84868204e4e7cf8cd069
kamalnayan <kamal.nayan@fantv.in>
2024-01-27 18:12:32 +0530

    Added placeholder

--- commons/src/main/java/com/kamalnayan/commons/utils/BindingAdapter.kt
+++ commons/src/main/java/com/kamalnayan/commons/utils/BindingAdapter.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.kamalnayan.commons.R
 
 /** @Author Kamal Nayan
 Created on: 26/01/24
@@ -12,14 +13,14 @@ Created on: 26/01/24
 
 @BindingAdapter("isVisible")
 fun setIsVisible(view: View, isVisible: Boolean) {
- view.isVisible = isVisible
+    view.isVisible = isVisible
 }
 
 
 @BindingAdapter("loadImageByUrl")
 fun loadImageByUrl(view: ImageView, url: String) {
- if (url.isNullOrEmpty())
-  return
+    if (url.isNullOrEmpty())
+        return
 
- Glide.with(view).load(url).into(view)
+    Glide.with(view).load(url).placeholder(R.drawable.ic_placeholder_wrapped).into(view)
 }
